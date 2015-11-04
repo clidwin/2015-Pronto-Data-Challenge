10dd425709e5ee4649e55009dd6c8c620c78ad69
Christina Lidwin <clidwin@gmail.com>
2015-11-03 23:14:34 -0800

    Skin Changes
    Updated:
    - Info Windows (additional text on stations) to show more content from
    the database
    - Functions within map.js for greater modularity
    
    Added:
    - External stylesheets
    - Site header and footer

--- pronto-viz/map.js
+++ pronto-viz/map.js
@@ -1,4 +1,32 @@
+var map;
+var stationData;
+
+/**
+ *
+ */
 function initMap() {
+  var rendererOptions = {
+      map: map,
+      preserveViewport: true,
+      suppressMarkers : true
+  };
+    
+  map = new google.maps.Map(document.getElementById('map-underlay'), {
+    zoom: 14,
+    center: {lat: 47.6097, lng: -122.3331},  // Seattle.
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+    }
+  });
+    
+  var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+  var directionsService = new google.maps.DirectionsService;
+    
+  // TODO(clidwin): Apply these boundaries to the map
+  var bounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(47.6095, -122.3330),
+    new google.maps.LatLng(47.6097, -122.3332)
+  );
   var customMapType = new google.maps.StyledMapType([
       {
         stylers: [
@@ -14,36 +42,67 @@ function initMap() {
       },
       {
         featureType: 'water',
-        stylers: [{color: '#003B49'}]
+        stylers: [{color: '#00708c'}]
       },
     ], {
       name: 'Pronto Style'
   });
   var customMapTypeId = 'pronto_style';
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
+    
+  initializeStations();
+    
+  directionsDisplay.setMap(map);
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
 
-  var map = new google.maps.Map(document.getElementById('map-underlay'), {
-    zoom: 14,
-    center: {lat: 47.6097, lng: -122.3331},  // Seattle.
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-    }
-  });
-  
-  var layer = new google.maps.FusionTablesLayer({
+/**
+ * Queries the database for the different Pronto bike stations and
+ * generates the content to be displayed when each station is clicked.
+ */
+function initializeStations() {
+  // Gather and draw all stations.
+  stationData = new google.maps.FusionTablesLayer({
     query: {
       select: 'lat',
-      from: '12mjOjVktZlJ6dTsFEC3_OVJYWCizYdE4o5XGQpPo'
+      from: '12mjOjVktZlJ6dTsFEC3_OVJYWCizYdE4o5XGQpPo' // TODO(clidwin): Scrub this information before launch
     },
     styles: [{
       markerOptions: {
-        iconName: "measle_turquoise" //cycling option for animations
+        iconName: 'measle_brown' //'cycling' option for animations
       }
     }]
   });
 
-  //TODO(clidwin): Animate trip data using the cycle marker
-  //TODO(clidwin): Format station information
-  map.mapTypes.set(customMapTypeId, customMapType);
-  map.setMapTypeId(customMapTypeId);
-  layer.setMap(map);
+  // Define the click action for each station
+  google.maps.event.addListener(stationData, 'click', function(e) {
+    // Set the content of the info window
+    // TODO(clidwin): Style this to match the color and typography in the rest of the map
+    e.infoWindowHtml = '<b>' + e.row['name'].value + '</b>' + '<br>';
+    e.infoWindowHtml += '<b>Terminal Id: </b>' + e.row['terminal'].value + '<br>';
+    e.infoWindowHtml += '<b>Online Date: </b>' + e.row['online'].value + '<br>';
+    e.infoWindowHtml += '<b>Dock Count: </b>' + e.row['dockcount'].value + '<br>';
+  });
+    
+  stationData.setMap(map);
+}
+
+/**
+ *
+ */
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    //TODO(clidwin): Add all routes and use cycle marker to animate
+    //https://stackoverflow.com/questions/6040965/drawing-multiple-route-google-map
+  directionsService.route({
+    origin: {lat: 47.618418, lng: -122.350964},
+    destination: {lat: 47.615829, lng: -122.348564},
+    travelMode: google.maps.TravelMode.BICYCLING
+  }, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }
